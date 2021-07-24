5b5da5401bf44686ef456dd7cf2351ca13203150
Yemisrach Serawit <se.yemisrach.serawit@gmail.com>
2021-07-24 18:39:12 +0300

    Change home page style

--- lib/dafaults/home.dart
+++ lib/dafaults/home.dart
@@ -10,17 +10,30 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildCard(context, 'assets/images/profile.jpg'),
-              buildCard(context, 'assets/images/profile.jpg'),
-            ],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: GridView.builder(
+              itemCount: 10,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 0.75, mainAxisSpacing: 20),
+              itemBuilder: (context, index) => buildCard(context, 'assets/images/profile.jpg'),
+            ),
           ),
-        ],
+        ),
       ),
+      // body: Column(
+      //   children: [
+      //     Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         buildCard(context, 'assets/images/profile.jpg'),
+      //         buildCard(context, 'assets/images/profile.jpg'),
+      //       ],
+      //     ),
+      //   ],
+      // ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xFF7BC74D),
         onPressed: () {
