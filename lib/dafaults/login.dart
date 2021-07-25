417d290e59af95cb3e09f67db4b01ae758c48b1d
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-07-25 01:11:03 -0700

    signup functionality

--- lib/dafaults/login.dart
+++ lib/dafaults/login.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
-class Login extends StatelessWidget {
+import 'package:blue_moon_flatter/dafaults/post.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../main.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final url = 'http://localhost:5000/api/v1';
+  // final url = 'http://192.168.1.248:5000/api/v1';
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,23 +27,64 @@ class Login extends StatelessWidget {
         child: Column(
           children: [
             TextFormField(
+              controller: emailController,
               decoration: InputDecoration(
                 hintText: 'email',
               ),
             ),
-            
             TextFormField(
+              controller: passwordController,
               decoration: InputDecoration(
                 hintText: 'password',
               ),
             ),
-            
             ElevatedButton(
-                onPressed: () => print('login'), child: Text('login')),
+                onPressed: () {
+                  Posts post = Posts(
+                      email: emailController.text,
+                      password: passwordController.text);
+                  createPost(url, body: post.toMap());
+                  
+                },
+                child: Text('login')),
           ],
         ),
       ),
     );
-    
+  }
+}
+
+Future createPost(String url, {required Map body}) async {
+  return http
+      .post(Uri.parse(url + "/user/login"),
+          headers: {'Content-Type': 'application/json'}, body: jsonEncode(body))
+      .then((http.Response response) {
+    // print(body['email'].runtimeType);
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    } 
+    print("Message returned: " + json.decode(response.body)['message']);
+    return Posts.fromJson(json.decode(response.body));
+  });
+}
+
+class Posts {
+  final String email;
+  final String password;
+
+  Posts({required this.email, required this.password});
+
+  factory Posts.fromJson(Map json) {
+    return Posts(email: json['email'] ?? "", password: json['password'] ?? "");
+  }
+
+  Map<String, String> toMap() {
+    var map = new Map<String, String>();
+    map["email"] = email;
+    map["password"] = password;
+
+    return map;
   }
 }
--- lib/dafaults/signup.dart
+++ lib/dafaults/signup.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:blue_moon_flatter/dafaults/login.dart';
 
-class SignUp extends StatelessWidget {
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final url = 'http://localhost:5000/api/v1';
+  // final url = 'http://192.168.1.248:5000/api/v1';
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final username = TextEditingController();
+  final phoneNumber = TextEditingController();
+  final confirmPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,35 +30,103 @@ class SignUp extends StatelessWidget {
         child: Column(
           children: [
             TextFormField(
+              controller: emailController,
               decoration: InputDecoration(
                 hintText: 'email',
               ),
             ),
             TextFormField(
+              controller: username,
               decoration: InputDecoration(
                 hintText: 'username',
               ),
             ),
             TextFormField(
+              controller: phoneNumber,
               decoration: InputDecoration(
                 hintText: 'phone number',
               ),
             ),
             TextFormField(
+              controller: passwordController,
               decoration: InputDecoration(
                 hintText: 'password',
               ),
             ),
             TextFormField(
+              controller: confirmPassword,
               decoration: InputDecoration(
                 hintText: 'confirm password',
               ),
             ),
             ElevatedButton(
-                onPressed: () => print('register'), child: Text('Register')),
+                onPressed: () {
+                  Posts post = Posts(
+                    email: emailController.text,
+                    password: passwordController.text,
+                    username: username.text,
+                    phoneNumber: phoneNumber.text,
+                    confirmPassword: confirmPassword.text,
+                  );
+                  createPost(url, body: post.toMap());
+                },
+                child: Text('Register')),
           ],
         ),
       ),
     );
+  }
+}
+
+Future createPost(String url, {required Map body}) async {
+  return http
+      .post(Uri.parse(url + "/user/register"),
+          headers: {'Content-Type': 'application/json'}, body: jsonEncode(body))
+      .then((http.Response response) {
+    // print(body['email'].runtimeType);
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    } else {
+      Login();
+    }
+    print("Message returned: " + json.decode(response.body)['username']);
+    return Posts.fromJson(json.decode(response.body));
+  });
+}
+
+class Posts {
+  final String email;
+  final String password;
+  final String username;
+  final String phoneNumber;
+  final String confirmPassword;
+
+  Posts(
+      {required this.email,
+      required this.password,
+      required this.username,
+      required this.phoneNumber,
+      required this.confirmPassword});
+
+  factory Posts.fromJson(Map json) {
+    return Posts(
+        email: json['email'] ?? "",
+        password: json['password'] ?? "",
+        username: json['usename'] ?? "",
+        phoneNumber: json['phoneNumber'] ?? "",
+        confirmPassword: json['confirmPassword'] ?? "");
+  }
+
+  Map<String, String> toMap() {
+    var map = new Map<String, String>();
+    map["email"] = email;
+    map["password"] = password;
+    map["username"] = username;
+    map["phoneNumber"] = phoneNumber;
+    map["confirmPassword"] = confirmPassword;
+
+    return map;
   }
 }
